1d6fefe0137435a57c8068b126890124e05a4da1
Azis <107441639+AzisHactiv8@users.noreply.github.com>
2023-04-01 15:04:12 +0700

    Update dag_args.py

--- dag_args.py
+++ dag_args.py
@@ -15,3 +15,55 @@ default_args = {
     'retry_delay':timedelta(minutes = 5)
 }
 
+# Task 1.2 - Define the DAG
+
+dag = DAG(
+    'ETL_toll_data',
+    default_args=default_args,
+    description='Apache Airflow Final Assignment',
+    schedule_interval=timedelta(days=1),
+)
+
+# define the tasks
+
+unzip_data = BashOperator(
+    task_id='unzip_data',
+    bash_command='tar -zxf /home/project/airflow/dags/finalassignment/tolldate.tgz',
+    dag=dag,
+)
+
+extract_data_from_csv = BashOperator(
+    task_id= 'extract_data_from_csv',
+    bash_command='cut -d"," -f1-4 vehicle-data.csv > csv_data.csv',
+    dag=dag,
+)
+
+extract_data_from_tsv = BashOperator(
+    task_id='extract_data_from_tsv',
+    bash_command='cut -f5-7 tollplaza-data.tsv > tsv_data.csv',
+    dag=dag,
+)
+
+extract_data_from_fixed_width = BashOperator(
+    task_id= 'extract_data_from_fixed_width',
+    bash_command= 'awk 'NF{print $(NF-1),$NF}' OFS="\t" payment-data.txt > fixed_width_data.csv',
+    dag = dag,
+)
+
+# Task 1.7 - Create a task to consolidate data extracted from previous tasks
+consolidate_data = BashOperator(
+    task_id = 'consolidate_data',
+    bash_command = 'paste csv_data.csv tsv_data.csv fixed_width_data.csv > extracted_data.csv',
+    dag = dag,
+)
+
+# Task 1.8 - Transform and load the data
+transform_data = BashOperator(
+    task_id = 'transform_data',
+    bash_command = 'awk '$5 = toupper($5)' < extracted_data.csv > transformed_data.csv',
+    dag = dag,
+)
+
+# Task 1.9 - Define The task pipeline
+unzip_data >> extract_data_from_csv >> extract_data_from_tsv >> extract_data_from_fixed_width >> consolidate_data >> transform_data
+
